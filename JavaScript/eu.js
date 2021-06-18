352be29fca6d96799158fe5ef91acf20711ff1c7
Wender Vega <wender_vega@hotmail.com>
2021-06-18 04:18:03 -0300

    primeiro commit

---
+++ JavaScript/eu.js
@@ -0,0 +1,21 @@
+cor = document.getElementById("cor");
+cor.onclick = function () {
+  cor = document.body.classList.toggle("darkMode");
+};
+
+/*
+ var icon = document.getElementById("icon");
+
+icon = function ta() {
+  document.body.classList.toggle("darkMode");
+};
+  
+
+
+var header1 = document.getElementById("header");
+var main1 = document.getElementById("main");
+var aside1 = document.getElementById("aside");
+var aside2 = document.getElementById("asside");
+var footer1 = document.getElementById("footer");
+
+*/
